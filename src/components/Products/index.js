b0b489c6fd1df8e196f0c329b811f46d6b826932
Edward Baldacchino <edward.baldacchino@live.vu.edu.au>
2021-06-29 19:15:12 +1000

    Formatted product price

--- src/components/Products/index.js
+++ src/components/Products/index.js
@@ -1,38 +1,44 @@
-import React from "react";
+import React from 'react';
 import {
-  ProductsContainer,
-  ProductsHeading,
-  ProductWrapper,
-  ProductCard,
-  ProductImg,
-  ProductInfo,
-  ProductTitle,
-  ProductDesc,
-  ProductPrice,
-  ProductButton,
-} from "./ProductsElements";
+	ProductsContainer,
+	ProductsHeading,
+	ProductWrapper,
+	ProductCard,
+	ProductImg,
+	ProductInfo,
+	ProductTitle,
+	ProductDesc,
+	ProductPrice,
+	ProductButton,
+} from './ProductsElements';
 
 function Products({ heading, data }) {
-  return (
-    <ProductsContainer>
-      <ProductsHeading>{heading}</ProductsHeading>
-      <ProductWrapper>
-        {data.map((product, index) => {
-          return (
-            <ProductCard key={index}>
-              <ProductImg src={product.img} alt={product.alt} />
-              <ProductInfo>
-                <ProductTitle>{product.name}</ProductTitle>
-                <ProductDesc>{product.desc}</ProductDesc>
-                <ProductPrice>{product.price}</ProductPrice>
-                <ProductButton>{product.button}</ProductButton>
-              </ProductInfo>
-            </ProductCard>
-          );
-        })}
-      </ProductWrapper>
-    </ProductsContainer>
-  );
+	return (
+		<ProductsContainer>
+			<ProductsHeading>{heading}</ProductsHeading>
+			<ProductWrapper>
+				{data.map((product, index) => {
+					const [dollars, cents] = product.price.split('.');
+					if (cents) {
+						if (cents.length === 1) {
+							product.price = `${dollars}.${cents}0`;
+						}
+					}
+					return (
+						<ProductCard key={index}>
+							<ProductImg src={product.img} alt={product.alt} />
+							<ProductInfo>
+								<ProductTitle>{product.name}</ProductTitle>
+								<ProductDesc>{product.desc}</ProductDesc>
+								<ProductPrice>{product.price}</ProductPrice>
+								<ProductButton>{product.button}</ProductButton>
+							</ProductInfo>
+						</ProductCard>
+					);
+				})}
+			</ProductWrapper>
+		</ProductsContainer>
+	);
 }
 
 export default Products;
